2d1ee3e3a96b2ce5be2b1f9e7bbd64965bbfa2b4
Felipe Gasper <felipe@felipegasper.com>
2026-01-13 10:43:10 -0500

    All types can now marshal/unmarshal all values.

--- bsontools/raw_value.go
+++ bsontools/raw_value.go
@@ -3,20 +3,33 @@ package bsontools
 import (
 	"fmt"
 	"math"
+	"slices"
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/x/bsonx/bsoncore"
 )
 
-type bsonCastRecipient interface {
-	// BSON types:
-	bson.Raw | bson.RawArray | bson.DateTime | bson.Decimal128 |
-		bson.Timestamp | bson.ObjectID | bson.Binary | bson.Regex |
-		bson.MinKey | bson.MaxKey | bson.Null | bson.Undefined |
+type alwaysMarshalableTypes interface {
+	float64 | string | bson.Raw | bson.RawArray | bson.Binary |
+		bson.Undefined | bson.ObjectID | bool | bson.DateTime | bson.Null |
+		bson.Regex | bson.DBPointer | bson.JavaScript | bson.Symbol |
 
-		// Go types:
-		bool | string | int32 | int64 | float64 | time.Time
+		// This type includes an `any`, which can’t always be marshaled.
+		//bson.CodeWithScope |
+
+		int32 | bson.Timestamp | int64 | bson.Decimal128 | bson.MinKey | bson.MaxKey
+}
+
+type bsonCastRecipient interface {
+	alwaysMarshalableTypes |
+
+		// This works for casting/unmarshaling but not for marshaling
+		// (because we don’t have to parse a Go `any` value when casting).
+		bson.CodeWithScope |
+
+		// Convenience:
+		time.Time
 }
 
 type cannotCastErr struct {
@@ -41,6 +54,14 @@ func RawValueTo[T bsonCastRecipient](in bson.RawValue) (T, error) {
 	var zero T
 
 	switch any(zero).(type) {
+	case float64:
+		if val, ok := in.DoubleOK(); ok {
+			return any(val).(T), nil
+		}
+	case string:
+		if str, ok := in.StringValueOK(); ok {
+			return any(str).(T), nil
+		}
 	case bson.Raw:
 		if doc, isDoc := in.DocumentOK(); isDoc {
 			return any(doc).(T), nil
@@ -49,66 +70,76 @@ func RawValueTo[T bsonCastRecipient](in bson.RawValue) (T, error) {
 		if arr, ok := in.ArrayOK(); ok {
 			return any(arr).(T), nil
 		}
-	case bson.DateTime:
-		if i64, ok := in.DateTimeOK(); ok {
-			return any(bson.DateTime(i64)).(T), nil
-		}
-	case bson.Decimal128:
-		if dec, ok := in.Decimal128OK(); ok {
-			return any(dec).(T), nil
-		}
-	case bson.Timestamp:
-		if t, i, ok := in.TimestampOK(); ok {
-			return any(bson.Timestamp{t, i}).(T), nil
-		}
-	case bson.ObjectID:
-		if oid, ok := in.ObjectIDOK(); ok {
-			return any(oid).(T), nil
-		}
 	case bson.Binary:
 		if subtype, buf, ok := in.BinaryOK(); ok {
 			return any(bson.Binary{subtype, buf}).(T), nil
-		}
-	case bson.Regex:
-		if pattern, opts, ok := in.RegexOK(); ok {
-			return any(bson.Regex{pattern, opts}).(T), nil
-		}
-	case bson.MinKey:
-		if in.Type == bson.TypeMinKey {
-			return any(zero).(T), nil
-		}
-	case bson.MaxKey:
-		if in.Type == bson.TypeMaxKey {
-			return any(zero).(T), nil
-		}
-	case bson.Null:
-		if in.Type == bson.TypeNull {
-			return any(zero).(T), nil
 		}
 	case bson.Undefined:
 		if in.Type == bson.TypeUndefined {
 			return any(zero).(T), nil
 		}
+	case bson.ObjectID:
+		if oid, ok := in.ObjectIDOK(); ok {
+			return any(oid).(T), nil
+		}
 	case bool:
 		if val, ok := in.BooleanOK(); ok {
 			return any(val).(T), nil
 		}
-	case string:
-		if str, ok := in.StringValueOK(); ok {
-			return any(str).(T), nil
+	case bson.DateTime:
+		if i64, ok := in.DateTimeOK(); ok {
+			return any(bson.DateTime(i64)).(T), nil
+		}
+	case bson.Null:
+		if in.Type == bson.TypeNull {
+			return any(zero).(T), nil
+		}
+	case bson.Regex:
+		if pattern, opts, ok := in.RegexOK(); ok {
+			return any(bson.Regex{pattern, opts}).(T), nil
+		}
+	case bson.DBPointer:
+		if db, ptr, ok := in.DBPointerOK(); ok {
+			return any(bson.DBPointer{db, ptr}).(T), nil
+		}
+	case bson.JavaScript:
+		if v, ok := in.JavaScriptOK(); ok {
+			return any(bson.JavaScript(v)).(T), nil
+		}
+	case bson.Symbol:
+		if v, ok := in.SymbolOK(); ok {
+			return any(bson.Symbol(v)).(T), nil
+		}
+	case bson.CodeWithScope:
+		if code, scope, ok := in.CodeWithScopeOK(); ok {
+			return any(bson.CodeWithScope{bson.JavaScript(code), scope}).(T), nil
 		}
 	case int32:
 		if val, ok := in.Int32OK(); ok {
 			return any(val).(T), nil
 		}
+	case bson.Timestamp:
+		if t, i, ok := in.TimestampOK(); ok {
+			return any(bson.Timestamp{t, i}).(T), nil
+		}
 	case int64:
 		if val, ok := in.Int64OK(); ok {
 			return any(val).(T), nil
 		}
-	case float64:
-		if val, ok := in.DoubleOK(); ok {
-			return any(val).(T), nil
+	case bson.Decimal128:
+		if dec, ok := in.Decimal128OK(); ok {
+			return any(dec).(T), nil
 		}
+	case bson.MaxKey:
+		if in.Type == bson.TypeMaxKey {
+			return any(zero).(T), nil
+		}
+	case bson.MinKey:
+		if in.Type == bson.TypeMinKey {
+			return any(zero).(T), nil
+		}
+
+	// convenience:
 	case time.Time:
 		if val, ok := in.TimeOK(); ok {
 			return any(val).(T), nil
@@ -121,10 +152,10 @@ func RawValueTo[T bsonCastRecipient](in bson.RawValue) (T, error) {
 }
 
 type bsonSourceTypes interface {
-	bson.ObjectID | bson.Raw |
-		bson.MinKey | bson.MaxKey | bson.Undefined | bson.Null |
+	alwaysMarshalableTypes |
 
-		string | int | int32 | int64 | float64
+		// For convenience:
+		int
 }
 
 // ToRawValue is a bit like bson.MarshalValue, but:
@@ -132,52 +163,108 @@ type bsonSourceTypes interface {
 // - It always succeeds since it only accepts certain known types.
 func ToRawValue[T bsonSourceTypes](in T) bson.RawValue {
 	switch typedIn := any(in).(type) {
-	case int:
-		if typedIn < math.MinInt32 || typedIn > math.MaxInt32 {
-			return i64ToRawValue(int64(typedIn))
-		}
-
-		return i32ToRawValue(typedIn)
-	case int32:
-		return i32ToRawValue(typedIn)
-	case int64:
-		return i64ToRawValue(typedIn)
 	case float64:
 		return bson.RawValue{
 			Type:  bson.TypeDouble,
 			Value: bsoncore.AppendDouble(nil, typedIn),
-		}
-	case bson.ObjectID:
-		return bson.RawValue{
-			Type:  bson.TypeObjectID,
-			Value: bsoncore.AppendObjectID(nil, typedIn),
-		}
-	case bson.Raw:
-		return bson.RawValue{
-			Type:  bson.TypeEmbeddedDocument,
-			Value: typedIn,
-		}
-	case bson.MinKey:
-		return bson.RawValue{
-			Type: bson.TypeMinKey,
-		}
-	case bson.MaxKey:
-		return bson.RawValue{
-			Type: bson.TypeMaxKey,
-		}
-	case bson.Undefined:
-		return bson.RawValue{
-			Type: bson.TypeUndefined,
-		}
-	case bson.Null:
-		return bson.RawValue{
-			Type: bson.TypeNull,
 		}
 	case string:
 		return bson.RawValue{
 			Type:  bson.TypeString,
 			Value: bsoncore.AppendString(nil, typedIn),
 		}
+	case bson.Raw:
+		return bson.RawValue{
+			Type:  bson.TypeEmbeddedDocument,
+			Value: typedIn,
+		}
+	case bson.RawArray:
+		return bson.RawValue{
+			Type:  bson.TypeArray,
+			Value: typedIn,
+		}
+	case bson.Binary:
+		return bson.RawValue{
+			Type:  bson.TypeBinary,
+			Value: bsoncore.AppendBinary(nil, typedIn.Subtype, typedIn.Data),
+		}
+	case bson.Undefined:
+		return bson.RawValue{
+			Type: bson.TypeUndefined,
+		}
+	case bson.ObjectID:
+		return bson.RawValue{
+			Type:  bson.TypeObjectID,
+			Value: bsoncore.AppendObjectID(nil, typedIn),
+		}
+	case bool:
+		return bson.RawValue{
+			Type:  bson.TypeBoolean,
+			Value: bsoncore.AppendBoolean(nil, typedIn),
+		}
+	case bson.DateTime:
+		return bson.RawValue{
+			Type:  bson.TypeDateTime,
+			Value: bsoncore.AppendDateTime(nil, int64(typedIn)),
+		}
+	case bson.Null:
+		return bson.RawValue{
+			Type: bson.TypeNull,
+		}
+	case bson.Regex:
+		// To match the driver’s marshaling logic:
+		opts := []rune(typedIn.Options)
+		slices.Sort(opts)
+
+		return bson.RawValue{
+			Type:  bson.TypeRegex,
+			Value: bsoncore.AppendRegex(nil, typedIn.Pattern, string(opts)),
+		}
+	case bson.DBPointer:
+		return bson.RawValue{
+			Type:  bson.TypeDBPointer,
+			Value: bsoncore.AppendDBPointer(nil, typedIn.DB, typedIn.Pointer),
+		}
+	case bson.JavaScript:
+		return bson.RawValue{
+			Type:  bson.TypeJavaScript,
+			Value: bsoncore.AppendJavaScript(nil, string(typedIn)),
+		}
+	case bson.Symbol:
+		return bson.RawValue{
+			Type:  bson.TypeSymbol,
+			Value: bsoncore.AppendSymbol(nil, string(typedIn)),
+		}
+	case int32:
+		return i32ToRawValue(typedIn)
+	case bson.Timestamp:
+		return bson.RawValue{
+			Type:  bson.TypeTimestamp,
+			Value: bsoncore.AppendTimestamp(nil, typedIn.T, typedIn.I),
+		}
+	case int64:
+		return i64ToRawValue(typedIn)
+	case bson.Decimal128:
+		h, l := typedIn.GetBytes()
+		return bson.RawValue{
+			Type:  bson.TypeDecimal128,
+			Value: bsoncore.AppendDecimal128(nil, h, l),
+		}
+	case bson.MaxKey:
+		return bson.RawValue{
+			Type: bson.TypeMaxKey,
+		}
+	case bson.MinKey:
+		return bson.RawValue{
+			Type: bson.TypeMinKey,
+		}
+
+	// For convenience:
+	case int:
+		if typedIn < math.MinInt32 || typedIn > math.MaxInt32 {
+			return i64ToRawValue(int64(typedIn))
+		}
+		return i32ToRawValue(typedIn)
 	}
 
 	panic(fmt.Sprintf("Unrecognized Go type: %T (maybe add marshal instructions?)", in))
--- bsontools/raw_value_test.go
+++ bsontools/raw_value_test.go
@@ -21,53 +21,7 @@ func TestWrongType(t *testing.T) {
 	assert.ErrorContains(t, err, fmt.Sprintf("%T", got))
 }
 
-func TestBool(t *testing.T) {
-	vals := []bool{false, true}
-
-	for _, cur := range vals {
-		viaMarshal := mustConvertToRawValue(t, cur)
-
-		assert.Equal(t, cur, lo.Must(RawValueTo[bool](viaMarshal)), "round-trip")
-	}
-}
-
-func TestInt32(t *testing.T) {
-	ints := []int32{
-		0,
-		-1,
-		math.MaxInt32 - 1,
-		math.MaxInt32,
-		math.MinInt32,
-		math.MinInt32 + 1,
-	}
-
-	for _, cur := range ints {
-		viaMarshal := mustConvertToRawValue(t, cur)
-
-		assert.Equal(t, cur, lo.Must(RawValueTo[int32](viaMarshal)), "round-trip")
-	}
-}
-
-func TestInt64(t *testing.T) {
-	ints := []int64{
-		0,
-		-1,
-		math.MaxInt32 - 1,
-		math.MaxInt32,
-		math.MaxInt32 + 1,
-		math.MaxInt64,
-		math.MinInt32 - 1,
-		math.MinInt32,
-		math.MinInt32 + 1,
-		math.MinInt64,
-	}
-
-	for _, cur := range ints {
-		viaMarshal := mustConvertToRawValue(t, cur)
-
-		assert.Equal(t, cur, lo.Must(RawValueTo[int64](viaMarshal)), "round-trip")
-	}
-}
+// The type tests below are in numeric BSON type order:
 
 func TestFloat64(t *testing.T) {
 	ints := []float64{
@@ -142,34 +96,17 @@ func TestRawArray(t *testing.T) {
 	}
 }
 
-func TestTimestamp(t *testing.T) {
-	vals := []bson.Timestamp{
-		{0, 0},
-		{1, 1},
-		{math.MaxUint32, math.MaxUint32},
+func TestBinary(t *testing.T) {
+	vals := []bson.Binary{
+		{0, []byte{}},
+		{0x4, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}},
 	}
 
 	for _, cur := range vals {
 		viaMarshal := mustConvertToRawValue(t, cur)
 
-		assert.Equal(t, cur, lo.Must(RawValueTo[bson.Timestamp](viaMarshal)))
+		assert.Equal(t, cur, lo.Must(RawValueTo[bson.Binary](viaMarshal)))
 	}
-}
-
-func TestMin(t *testing.T) {
-	fromUs := ToRawValue(bson.MinKey{})
-	viaMarshal := mustConvertToRawValue(t, bson.MinKey{})
-
-	assert.Equal(t, bson.MinKey{}, lo.Must(RawValueTo[bson.MinKey](fromUs)))
-	assert.Equal(t, bson.MinKey{}, lo.Must(RawValueTo[bson.MinKey](viaMarshal)))
-}
-
-func TestMax(t *testing.T) {
-	fromUs := ToRawValue(bson.MaxKey{})
-	viaMarshal := mustConvertToRawValue(t, bson.MaxKey{})
-
-	assert.Equal(t, bson.MaxKey{}, lo.Must(RawValueTo[bson.MaxKey](fromUs)))
-	assert.Equal(t, bson.MaxKey{}, lo.Must(RawValueTo[bson.MaxKey](viaMarshal)))
 }
 
 func TestUndefined(t *testing.T) {
@@ -178,14 +115,6 @@ func TestUndefined(t *testing.T) {
 
 	assert.Equal(t, bson.Undefined{}, lo.Must(RawValueTo[bson.Undefined](fromUs)))
 	assert.Equal(t, bson.Undefined{}, lo.Must(RawValueTo[bson.Undefined](viaMarshal)))
-}
-
-func TestNull(t *testing.T) {
-	fromUs := ToRawValue(bson.Null{})
-	viaMarshal := mustConvertToRawValue(t, bson.Null{})
-
-	assert.Equal(t, bson.Null{}, lo.Must(RawValueTo[bson.Null](fromUs)))
-	assert.Equal(t, bson.Null{}, lo.Must(RawValueTo[bson.Null](viaMarshal)))
 }
 
 func TestObjectID(t *testing.T) {
@@ -198,6 +127,16 @@ func TestObjectID(t *testing.T) {
 		viaMarshal := mustConvertToRawValue(t, cur)
 
 		assert.Equal(t, cur, lo.Must(RawValueTo[bson.ObjectID](viaMarshal)))
+	}
+}
+
+func TestBool(t *testing.T) {
+	vals := []bool{false, true}
+
+	for _, cur := range vals {
+		viaMarshal := mustConvertToRawValue(t, cur)
+
+		assert.Equal(t, cur, lo.Must(RawValueTo[bool](viaMarshal)), "round-trip")
 	}
 }
 
@@ -229,6 +168,129 @@ func TestDateTime(t *testing.T) {
 	}
 }
 
+func TestNull(t *testing.T) {
+	fromUs := ToRawValue(bson.Null{})
+	viaMarshal := mustConvertToRawValue(t, bson.Null{})
+
+	assert.Equal(t, bson.Null{}, lo.Must(RawValueTo[bson.Null](fromUs)))
+	assert.Equal(t, bson.Null{}, lo.Must(RawValueTo[bson.Null](viaMarshal)))
+}
+
+func TestRegex(t *testing.T) {
+	vals := []bson.Regex{
+		{},
+		{Pattern: "abc", Options: "i"},
+	}
+
+	for _, cur := range vals {
+		viaMarshal := mustConvertToRawValue(t, cur)
+
+		assert.Equal(t, cur, lo.Must(RawValueTo[bson.Regex](viaMarshal)))
+	}
+}
+
+func TestDBPointer(t *testing.T) {
+	for _, val := range []bson.DBPointer{
+		{},
+		{DB: "abc", Pointer: bson.NewObjectID()},
+	} {
+		fromUs := ToRawValue(val)
+		viaMarshal := mustConvertToRawValue(t, val)
+
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.DBPointer](fromUs)))
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.DBPointer](viaMarshal)))
+	}
+}
+
+func TestJavaScript(t *testing.T) {
+	for _, val := range []bson.JavaScript{
+		"",
+		"abc",
+	} {
+		fromUs := ToRawValue(val)
+		viaMarshal := mustConvertToRawValue(t, val)
+
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.JavaScript](fromUs)))
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.JavaScript](viaMarshal)))
+	}
+}
+
+func TestSymbol(t *testing.T) {
+	for _, val := range []bson.Symbol{
+		"",
+		"abc",
+	} {
+		fromUs := ToRawValue(val)
+		viaMarshal := mustConvertToRawValue(t, val)
+
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.Symbol](fromUs)))
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.Symbol](viaMarshal)))
+	}
+}
+
+func TypeCodeWithScope(t *testing.T) {
+	for _, val := range []bson.CodeWithScope{
+		{},
+		{Code: "abc", Scope: bson.D{{"foo", "bar"}}},
+	} {
+		viaMarshal := mustConvertToRawValue(t, val)
+
+		assert.Equal(t, val, lo.Must(RawValueTo[bson.CodeWithScope](viaMarshal)))
+	}
+}
+
+func TestInt32(t *testing.T) {
+	ints := []int32{
+		0,
+		-1,
+		math.MaxInt32 - 1,
+		math.MaxInt32,
+		math.MinInt32,
+		math.MinInt32 + 1,
+	}
+
+	for _, cur := range ints {
+		viaMarshal := mustConvertToRawValue(t, cur)
+
+		assert.Equal(t, cur, lo.Must(RawValueTo[int32](viaMarshal)), "round-trip")
+	}
+}
+
+func TestTimestamp(t *testing.T) {
+	vals := []bson.Timestamp{
+		{0, 0},
+		{1, 1},
+		{math.MaxUint32, math.MaxUint32},
+	}
+
+	for _, cur := range vals {
+		viaMarshal := mustConvertToRawValue(t, cur)
+
+		assert.Equal(t, cur, lo.Must(RawValueTo[bson.Timestamp](viaMarshal)))
+	}
+}
+
+func TestInt64(t *testing.T) {
+	ints := []int64{
+		0,
+		-1,
+		math.MaxInt32 - 1,
+		math.MaxInt32,
+		math.MaxInt32 + 1,
+		math.MaxInt64,
+		math.MinInt32 - 1,
+		math.MinInt32,
+		math.MinInt32 + 1,
+		math.MinInt64,
+	}
+
+	for _, cur := range ints {
+		viaMarshal := mustConvertToRawValue(t, cur)
+
+		assert.Equal(t, cur, lo.Must(RawValueTo[int64](viaMarshal)), "round-trip")
+	}
+}
+
 func TestDecimal128(t *testing.T) {
 	vals := []bson.Decimal128{
 		{},
@@ -243,30 +305,20 @@ func TestDecimal128(t *testing.T) {
 	}
 }
 
-func TestBinary(t *testing.T) {
-	vals := []bson.Binary{
-		{0, []byte{}},
-		{0x4, []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}},
-	}
+func TestMax(t *testing.T) {
+	fromUs := ToRawValue(bson.MaxKey{})
+	viaMarshal := mustConvertToRawValue(t, bson.MaxKey{})
 
-	for _, cur := range vals {
-		viaMarshal := mustConvertToRawValue(t, cur)
-
-		assert.Equal(t, cur, lo.Must(RawValueTo[bson.Binary](viaMarshal)))
-	}
+	assert.Equal(t, bson.MaxKey{}, lo.Must(RawValueTo[bson.MaxKey](fromUs)))
+	assert.Equal(t, bson.MaxKey{}, lo.Must(RawValueTo[bson.MaxKey](viaMarshal)))
 }
 
-func TestRegex(t *testing.T) {
-	vals := []bson.Regex{
-		{},
-		{Pattern: "abc", Options: "i"},
-	}
+func TestMin(t *testing.T) {
+	fromUs := ToRawValue(bson.MinKey{})
+	viaMarshal := mustConvertToRawValue(t, bson.MinKey{})
 
-	for _, cur := range vals {
-		viaMarshal := mustConvertToRawValue(t, cur)
-
-		assert.Equal(t, cur, lo.Must(RawValueTo[bson.Regex](viaMarshal)))
-	}
+	assert.Equal(t, bson.MinKey{}, lo.Must(RawValueTo[bson.MinKey](fromUs)))
+	assert.Equal(t, bson.MinKey{}, lo.Must(RawValueTo[bson.MinKey](viaMarshal)))
 }
 
 func mustConvertToRawValue(t *testing.T, val any) bson.RawValue {
