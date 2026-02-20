24184b1464a26e1afe0565bbf035264890ead967
Felipe Gasper <felipe@felipegasper.com>
2026-02-20 15:18:43 -0500

    add future, repackaged & tweaked from Migration Verifier’s eventual

---
+++ future/future_test.go
@@ -0,0 +1,65 @@
+package util
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEventual(t *testing.T) {
+	eventual, setter := New[int]()
+
+	assert.Panics(t, func() { eventual.Get() },
+		"Get() should panic before the value is set",
+	)
+
+	select {
+	case <-eventual.Ready():
+		require.Fail(t, "should not be ready")
+	case <-time.NewTimer(time.Millisecond).C:
+	}
+
+	setter(123)
+
+	select {
+	case <-eventual.Ready():
+	case <-time.NewTimer(time.Millisecond).C:
+		require.Fail(t, "should be ready")
+	}
+
+	assert.Equal(
+		t,
+		123,
+		eventual.Get(),
+		"Get() should return the value",
+	)
+
+	assert.Equal(
+		t,
+		123,
+		eventual.Get(),
+		"Get() should return the value a 2nd time",
+	)
+}
+
+func TestEventualNil(t *testing.T) {
+	eventual, setter := New[error]()
+
+	select {
+	case <-eventual.Ready():
+		require.Fail(t, "should not be ready")
+	case <-time.NewTimer(time.Millisecond).C:
+	}
+
+	setter(nil)
+
+	select {
+	case <-eventual.Ready():
+	case <-time.NewTimer(time.Millisecond).C:
+		require.Fail(t, "should be ready")
+	}
+
+	assert.Nil(t, eventual.Get())
+}
