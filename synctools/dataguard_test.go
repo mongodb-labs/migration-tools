e62329168e4c4612bb581a2102c247795f26b355
Felipe Gasper <FGasper@users.noreply.github.com>
2026-03-20 09:27:45 -0400

    Add DataGuard (#16)

---
+++ synctools/dataguard_test.go
@@ -0,0 +1,51 @@
+package synctools
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type unitTestSuite struct {
+	suite.Suite
+}
+
+func TestUnitTestSuite(t *testing.T) {
+	suite.Run(t, &unitTestSuite{})
+}
+
+func (s *unitTestSuite) TestDataGuard_Zero() {
+	l := &DataGuard[int]{}
+
+	l.Load(func(v int) {
+		s.Assert().Equal(0, v)
+	})
+}
+
+func (s *unitTestSuite) TestDataGuard() {
+	l := NewDataGuard(42)
+
+	var wg sync.WaitGroup
+	for i := range 100 {
+		var delta int
+		if i%2 == 0 {
+			delta = 2
+		} else {
+			delta = -1
+		}
+
+		wg.Go(func() {
+			l.Store(func(v int) int {
+				return v + delta
+			})
+		})
+	}
+	wg.Wait()
+
+	l.Load(func(v int) {
+		s.Require().Equal(92, v)
+	})
+
+	s.Require().Equal(92, l.CopyValue())
+}
