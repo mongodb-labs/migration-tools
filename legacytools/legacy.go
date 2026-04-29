f8b177238985a32384594a4ed79504968bfb18a9
Felipe Gasper <felipe@felipegasper.com>
2026-04-29 16:12:57 -0400

    older

--- legacytools/legacy.go
+++ legacytools/legacy.go
@@ -5,8 +5,10 @@ import (
 	"go.mongodb.org/mongo-driver/v2/x/mongo/driver/topology"
 )
 
+// cf. https://github.com/mongodb/specifications/blob/b519824da64005cdf99ca680fc49c4e278af0ef3/source/wireversion-featurelist/wireversion-featurelist.md
 var dbWireVersion = map[string]int32{
 	"4.0": 7,
+	"4.2": 8,
 }
 
 // SetDriverCompatibility alters the driver’s global state so that it will
