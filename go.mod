19902b11adb5494241d88652f0e1754fc7a11ed6
Felipe Gasper <felipe@felipegasper.com>
2026-04-29 15:05:38 -0400

    save

--- go.mod
+++ go.mod
@@ -12,9 +12,9 @@ require (
 	github.com/shirou/gopsutil/v4 v4.26.3
 	github.com/stretchr/testify v1.11.1
 	github.com/wI2L/jsondiff v0.7.0
-	go.mongodb.org/mongo-driver/v2 v2.5.0
+	go.mongodb.org/mongo-driver/v2 v2.6.0
 	golang.org/x/exp v0.0.0-20260211191109-2735e65f0518
-	golang.org/x/sync v0.19.0
+	golang.org/x/sync v0.20.0
 )
 
 require (
@@ -22,7 +22,7 @@ require (
 	github.com/ebitengine/purego v0.10.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/jaypipes/pcidb v1.1.1 // indirect
-	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/klauspost/compress v1.18.5 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
@@ -35,9 +35,11 @@ require (
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.2.0 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/sys v0.41.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/crypto v0.50.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	howett.net/plist v1.0.2-0.20250314012144-ee69052608d9 // indirect
 )
---
+++ mongotools/session/session.go
@@ -0,0 +1,77 @@
+// Package session exports useful tools for MongoDB sessions.
+package session
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/mongodb-labs/migration-tools/bsontools"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/x/bsonx/bsoncore"
+)
+
+const (
+	staleClusterTimeErrCode   = 209
+	opTimeKeyInServerResponse = "operationTime"
+	dollarClusterTime         = "$clusterTime"
+)
+
+func BootstrapCausalConsistency(
+	ctx context.Context,
+	sess *mongo.Session,
+) error {
+	resp, err := sess.Client().Database("admin").RunCommand(
+		ctx,
+		bson.D{
+			{"appendOplogNote", 1},
+			{"data", bson.D{
+				{"bootstrap", true},
+			}},
+			{"writeConcern", bson.D{{"w", "majority"}}},
+		},
+	).Raw()
+	if err != nil {
+		// If any shard’s cluster time >= maxTime, the mongos will return a
+		// StaleClusterTime error. This particular error doesn’t indicate a
+		// failure, so we ignore it.
+		if !slices.Contains(mongo.ErrorCodes(err), staleClusterTimeErrCode) {
+			return fmt.Errorf("appendOplogNote: %w", err)
+		}
+	}
+
+	opTime, err := bsontools.RawLookup[bson.Timestamp](resp, opTimeKeyInServerResponse)
+	if err != nil {
+		return fmt.Errorf("read %q in server response: %w", opTimeKeyInServerResponse, err)
+	}
+
+	if err := sess.AdvanceOperationTime(&opTime); err != nil {
+		return fmt.Errorf("advance session operation time: %w", err)
+	}
+
+	ctVal, err := resp.LookupErr(dollarClusterTime)
+	if err != nil {
+		return fmt.Errorf("read %q in server response: %w", dollarClusterTime, err)
+	}
+
+	clusterTime := bson.Raw(
+		bsoncore.BuildDocumentFromElements(
+			nil,
+			bsoncore.AppendValueElement(
+				nil,
+				"$clusterTime",
+				bsoncore.Value{
+					Type: bsoncore.Type(ctVal.Type),
+					Data: ctVal.Value,
+				},
+			),
+		),
+	)
+
+	if err := sess.AdvanceClusterTime(clusterTime); err != nil {
+		return fmt.Errorf("advance session cluster time: %w", err)
+	}
+
+	return nil
+}
