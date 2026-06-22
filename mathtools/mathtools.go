4853bcec68efa01070a6ff6cae72080ada636fba
Felipe Gasper <FGasper@users.noreply.github.com>
2026-06-22 14:47:13 -0400

    Add a mathtools.Mean() function to generate floating-point averages (#39)
    `lo.Mean()` exists but returns the same type as the given values. This function returns a float64 instead.

---
+++ mathtools/mathtools.go
@@ -0,0 +1,25 @@
+package mathtools
+
+import (
+	"github.com/mongodb-labs/migration-tools/option"
+	"github.com/samber/lo"
+	"golang.org/x/exp/constraints"
+)
+
+// RealNumber is implemented by any real-number type.
+type RealNumber interface {
+	constraints.Integer | constraints.Float
+}
+
+// Mean returns the arithmetic mean of all numbers in the slice,
+// or None if the slice is empty.
+func Mean[T RealNumber](nums []T) option.Option[float64] {
+	if len(nums) == 0 {
+		return option.None[float64]()
+	}
+
+	// To avoid integer overflow, convert all values to floats before summing.
+	mean := lo.MeanBy(nums, func(n T) float64 { return float64(n) })
+
+	return option.Some(mean)
+}
